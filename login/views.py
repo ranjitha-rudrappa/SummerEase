eab7e47d8bcb9c42774a1cc81973da70d73e3e8f
ashmit <ashmitsinha806@gmail.com>
2024-02-02 09:44:30 +0530

    deploy commit

--- login/views.py
+++ login/views.py
@@ -187,7 +187,7 @@ def login(request):
             request.session['user_id'] = user.id
             return redirect('index')
         else:
-            error_message = "Invalid username or password."
+            error_message = "Invalid username o r password."
             return render(request, 'login.html', {'error_message': error_message})
 
     return render(request, 'login.html')
